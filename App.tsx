8fb31c65e63088a8cf608b28f6ea9aa3841d53bc
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-26 07:44:51 -0300

    install theme gluestack

--- App.tsx
+++ App.tsx
@@ -1,7 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
+import { 
+  useFonts,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter'
+
+import { 
+  Poppins_400Regular,
+} from '@expo-google-fonts/poppins'
+
 export default function App() {
+  const [ fontsLoaded ] = useFonts({Inter_700Bold, Poppins_400Regular})
+
   return (
     <View>
       <Text>Open up App.tsx to start working on your app!</Text>
---
+++ src/screens/Login.tsx
@@ -0,0 +1,11 @@
+import { VStack, Text } from "@gluestack-ui/themed";
+
+export function Login() {
+  return (
+    <VStack flex={1}>
+      <Text color="$gray200" textTransform="capitalize">
+        Login
+      </Text>
+    </VStack>
+  )
+}
